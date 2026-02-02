229d0acc7b7028aeb8351e029add50ab266c75f1
jaimzh <jameshenshaw10@gmail.com>
2026-02-02 05:36:14 +0100

    fix: added fallback to about info in case cms fails

--- components/sections/about/about-info.tsx
+++ components/sections/about/about-info.tsx
@@ -1,40 +1,22 @@
-"use client";
-
 import React from "react";
-import { motion } from "framer-motion";
-import { Button } from "@/components/ui/button";
-import { LottieAndSVG } from "@/components/ui/lottie-and-svg";
-import { containerVariants, itemVariants } from "@/lib/animations";
-import { Headphones, Mic2Icon, Music4Icon, Signature } from "lucide-react";
-import { RolesList } from "./roles-list";
-import { SkillsList } from "./skills-list";
-import { CalebSignatureStroke } from "./caleb-signature";
-import { useScrollTo } from "@/hooks/useScrollTo";
-
-import { useEffect, useState } from "react";
 import { client } from "@/sanity/lib/client";
 import { AboutInfoClient } from "./about-info-client";
 
-export function AboutInfo() {
-  const { scrollToId } = useScrollTo();
+export async function AboutInfo() {
+  let description =
+    "Caleb Caz is a New York–based voice actor with a focus in audiobooks, commercials video games, animations and more. Beyond the mic, he crafts original music jingles, immersive soundscapes, and foley. Collaborations include media companies Sling TV, Genshin Impact as well as YouTubers Comical Realm Animations and Slug Films alongside creators such as Jaimz Art, JollyShow and 360 Animations.";
 
-  const [description, setDescription] = useState("");
+  try {
+    const query = `*[_type == "aboutInfo"][0]{description}`;
+    const data = await client.fetch(query, {}, { cache: "no-store" });
 
-  useEffect(() => {
-    const fetchAboutInfo = async () => {
-      const query = `*[_type == "aboutInfo"][0]{description}`;
+    if (data?.description) {
+      description = data.description;
+    }
+  } catch (error) {
+    console.error("Failed to fetch About Info:", error);
+    // Keep default fallback description
+  }
 
-      const data = await client.fetch(query);
-
-      if (data?.description) {
-        setDescription(data.description);
-      }
-    };
-
-    fetchAboutInfo();
-  }, []);
-
-  return (
-   <AboutInfoClient description={description} />
-  );
+  return <AboutInfoClient description={description} />;
 }
